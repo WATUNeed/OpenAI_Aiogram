8b87b9779976943c69cb83b77c80abf46d104d72
WATUNEED <watyneed@gmail.com>
2023-02-28 21:14:00 +0400

    add_on_validate test

--- tests/test_get_html_markup.py
+++ tests/test_get_html_markup.py
@@ -3,16 +3,11 @@ import sys
 
 import pytest
 
-from websites import CryptoSlate, get_html_markup
+from websites import Websites
 
 import asyncio
 
 import platform
 
 
-@pytest.mark.parametrize('expect_response', [b'<!DOCTYPE html><html lang="en-US"><head> <script async src=', ])
-def test_correct_cases(expect_response):
-    if platform.system() == 'Windows':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    result = asyncio.run(get_html_markup(CryptoSlate()))
-    assert expect_response == result.encode(sys.stdout.encoding, errors='replace')[:59]
+
---
+++ tests/test_on_validate.py
@@ -0,0 +1,16 @@
+import pytest
+
+from websites import Websites
+
+import asyncio
+
+
+@pytest.mark.parametrize('condition, e', [(True, 'Exception error!'), (True, 1)])
+def test_standard_cases(condition: bool, e: str):
+    with pytest.raises(Exception):
+        asyncio.run(Websites().on_validate(condition, e))
+
+
+@pytest.mark.parametrize('condition, e', [(False, 'Exception error!')])
+def test_nostandard_cases(condition: bool, e: str):
+    asyncio.run(Websites().on_validate(condition, e))
